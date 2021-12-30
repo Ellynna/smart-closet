5f480ff8cf748e4c2c00b678bc5318649ddbd3d2
Ellynna <88528515+Ellynna@users.noreply.github.com>
2021-12-31 02:01:47 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import json
+import sys
+import os
 
-app = Flask(__name__)
+application = Flask(__name__, static_folder='static')
+# @application.route("/")
+# def hello():
+#     return render_template("home.html")
 
-@app.route('/main/')
-def mainpage():
-    return render_template('main.html')
+# @application.route("/")
+# def hello():
+#     return render_template("closet.html")
 
-@app.route('/closet/')
-def closetpage():
-    return render_template('closet.html')
+@application.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/ootd/')
-def ootdpage():
-    return "ootd page"
+@application.route("/closet")
+def closet():
+    return render_template("closet.html")
 
-@app.route('/codi/')
-def codipage():
-    return "codi page"
+@application.route("/closet_1")
+def closet_1():
+    #filenames = os.listdir('static/images/c1')
+    with open('clothes.json') as cloth_json:
+        json_data = json.load(cloth_json)
+        box1_data = json_data["closet"][0]
+    return render_template("closet_1.html",result=box1_data)
+
+
+
+# @application.route("/closet_detail")
+# def closet_detail():
+#     return render_template("closet_detail.html")
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
